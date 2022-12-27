3613500616b59daf7a1620623a50df28b87b6787
q <q>
2022-12-27 01:09:11 -0800

    add service feature

--- order_service/internal/service/contract.go
+++ order_service/internal/service/contract.go
@@ -5,4 +5,5 @@ import "order_service/internal/model"
 type store interface {
 	GetOrderById(id string) *model.Order
 	UploadCache() error
+	Set(order model.Order) error
 }
--- order_service/internal/service/service.go
+++ order_service/internal/service/service.go
@@ -20,4 +20,12 @@ func (s *Service) ReciveFromChan() {
 	if err := s.store.UploadCache(); err != nil {
 		log.Fatalf("ReciveFromChan: err %s", err)
 	}
+
+	for {
+		order := <-s.ch
+
+		if err := s.store.Set(order); err != nil {
+			log.Printf("ReciveFromChan: Set cannot insert order %s into store err: %s", order.OrderUID, err)
+		}
+	}
 }
